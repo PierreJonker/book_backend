381f78b32612b915357bf763d7830e78b743af28
Pierre <jonkerpierre637@gmail.com>
2024-06-12 14:46:26 +0200

    Backend

---
+++ config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://Pierre:jonker55@mern.lr0r4rq.mongodb.net/books-collection?retryWrites=true&w=majority&appName=MERN';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
